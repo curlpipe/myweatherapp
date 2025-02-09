b7eb46e6c2d8761f5e49ed32692083e3460aedae
Luke <11898833+curlpipe@users.noreply.github.com>
2025-02-09 15:03:48 +0000

    added spotless tool to maven for code formatting

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -1,10 +1,10 @@
 package com.weatherapp.myweatherapp.controller;
 
-import java.time.LocalTime;
-import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 import com.weatherapp.myweatherapp.model.CityInfo;
 import com.weatherapp.myweatherapp.service.WeatherService;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,52 +15,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WeatherController {
 
-  @Autowired
-  WeatherService weatherService;
+    @Autowired WeatherService weatherService;
 
-  @GetMapping("/forecast/{city}")
-  public ResponseEntity<CityInfo> forecastByCity(@PathVariable("city") String city) {
+    @GetMapping("/forecast/{city}")
+    public ResponseEntity<CityInfo> forecastByCity(@PathVariable("city") String city) {
 
-    CityInfo ci = weatherService.forecastByCity(city);
+        CityInfo ci = weatherService.forecastByCity(city);
 
-    return ResponseEntity.ok(ci);
-  }
+        return ResponseEntity.ok(ci);
+    }
 
-  /**
-  * This method serves as the endpoint that, between two cities,
-  * finds the one that has the longest daylight hours
-  *
-  * Example query: GET /longest_daylight?city1=London&city2=Lagos
-  * Note that arguments come from HTTP arguments in the URL
-  * 
-  * @param city1 This is the first city to compare, you can provide a name, zip code or coordinates
-  * @param city2 This is the second city to compare, you can provide a name, zip code or coordinates
-  * @return The response to show in the browser, in the format of a single string with the city name
-  */
-  @GetMapping("/longest_daylight")
-  public ResponseEntity<String> longestDaylight(@RequestParam String city1, @RequestParam String city2) {
-      // Retrieve the data for the two cities
-      CityInfo city1Info = weatherService.forecastByCity(city1);
-      CityInfo city2Info = weatherService.forecastByCity(city2);
-      
-      // Parse sunrise/sunset time data
-      DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
-      LocalTime city1Sunrise = LocalTime.parse(city1Info.currentConditions.sunrise, timeFormat);
-      LocalTime city1Sunset = LocalTime.parse(city1Info.currentConditions.sunset, timeFormat);
-      LocalTime city2Sunrise = LocalTime.parse(city2Info.currentConditions.sunrise, timeFormat);
-      LocalTime city2Sunset = LocalTime.parse(city2Info.currentConditions.sunset, timeFormat);
-      
-      // Work out the duration between sunrise and sunset for both cities
-      long city1Daylight = Duration.between(city1Sunrise, city1Sunset).toSeconds();
-      long city2Daylight = Duration.between(city2Sunrise, city2Sunset).toSeconds();
-      
-      // Return the city with the longest period between sunrise and sunset
-      if (city1Daylight > city2Daylight) {
-          // City 1 has more daylight
-          return ResponseEntity.ok(city1);
-      } else {
-          // City 2 has more daylight
-          return ResponseEntity.ok(city2);
-      }
-  }
+    /**
+     * This method serves as the endpoint that, between two cities,
+     * finds the one that has the longest daylight hours
+     *
+     * Example query: GET /longest_daylight?city1=London&city2=Lagos
+     * Note that arguments come from HTTP arguments in the URL
+     *
+     * @param city1 This is the first city to compare, you can provide a name, zip code or coordinates
+     * @param city2 This is the second city to compare, you can provide a name, zip code or coordinates
+     * @return The response to show in the browser, in the format of a single string with the city name
+     */
+    @GetMapping("/longest_daylight")
+    public ResponseEntity<String> longestDaylight(
+            @RequestParam String city1, @RequestParam String city2) {
+        // Retrieve the data for the two cities
+        CityInfo city1Info = weatherService.forecastByCity(city1);
+        CityInfo city2Info = weatherService.forecastByCity(city2);
+
+        // Parse sunrise/sunset time data
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime city1Sunrise = LocalTime.parse(city1Info.currentConditions.sunrise, timeFormat);
+        LocalTime city1Sunset = LocalTime.parse(city1Info.currentConditions.sunset, timeFormat);
+        LocalTime city2Sunrise = LocalTime.parse(city2Info.currentConditions.sunrise, timeFormat);
+        LocalTime city2Sunset = LocalTime.parse(city2Info.currentConditions.sunset, timeFormat);
+
+        // Work out the duration between sunrise and sunset for both cities
+        long city1Daylight = Duration.between(city1Sunrise, city1Sunset).toSeconds();
+        long city2Daylight = Duration.between(city2Sunrise, city2Sunset).toSeconds();
+
+        // Return the city with the longest period between sunrise and sunset
+        if (city1Daylight > city2Daylight) {
+            // City 1 has more daylight
+            return ResponseEntity.ok(city1);
+        } else {
+            // City 2 has more daylight
+            return ResponseEntity.ok(city2);
+        }
+    }
 }
--- src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
+++ src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
@@ -7,56 +7,56 @@ import java.util.List;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class CityInfo {
 
-  @JsonProperty("address")
-  public String address;
-
-  @JsonProperty("description")
-  public String description;
-
-  @JsonProperty("currentConditions")
-  public CurrentConditions currentConditions;
-
-  @JsonProperty("days")
-  public List<Days> days;
-
-  public static class CurrentConditions {
-    @JsonProperty("temp")
-    public String currentTemperature;
-
-    @JsonProperty("sunrise")
-    public String sunrise;
-
-    @JsonProperty("sunset")
-    public String sunset;
-
-    @JsonProperty("feelslike")
-    public String feelslike;
-
-    @JsonProperty("humidity")
-    public String humidity;
-
-    @JsonProperty("conditions")
-    public String conditions;
-  }
-
-  public static class Days {
-
-    @JsonProperty("datetime")
-    public String date;
-
-    @JsonProperty("temp")
-    public String currentTemperature;
-
-    @JsonProperty("tempmax")
-    public String maxTemperature;
-
-    @JsonProperty("tempmin")
-    public String minTemperature;
-
-    @JsonProperty("conditions")
-    public String conditions;
+    @JsonProperty("address")
+    public String address;
 
     @JsonProperty("description")
     public String description;
-  }
+
+    @JsonProperty("currentConditions")
+    public CurrentConditions currentConditions;
+
+    @JsonProperty("days")
+    public List<Days> days;
+
+    public static class CurrentConditions {
+        @JsonProperty("temp")
+        public String currentTemperature;
+
+        @JsonProperty("sunrise")
+        public String sunrise;
+
+        @JsonProperty("sunset")
+        public String sunset;
+
+        @JsonProperty("feelslike")
+        public String feelslike;
+
+        @JsonProperty("humidity")
+        public String humidity;
+
+        @JsonProperty("conditions")
+        public String conditions;
+    }
+
+    public static class Days {
+
+        @JsonProperty("datetime")
+        public String date;
+
+        @JsonProperty("temp")
+        public String currentTemperature;
+
+        @JsonProperty("tempmax")
+        public String maxTemperature;
+
+        @JsonProperty("tempmin")
+        public String minTemperature;
+
+        @JsonProperty("conditions")
+        public String conditions;
+
+        @JsonProperty("description")
+        public String description;
+    }
 }
